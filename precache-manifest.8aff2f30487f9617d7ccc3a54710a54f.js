342d85597b717336602b1b403e8b09fd287f32e3
Henri Parviainen <henri.parviainen@icloud.com>
2020-05-02 10:04:04 +0300

    Updates

---
+++ precache-manifest.8aff2f30487f9617d7ccc3a54710a54f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee6ef89fd8a81ede1aae3c4124bd1367",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "15e98637dc98a5a8bf6c",
+    "url": "/monsters-rolodex/static/css/main.d6feff52.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15e98637dc98a5a8bf6c",
+    "url": "/monsters-rolodex/static/js/main.8119c280.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
